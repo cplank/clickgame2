8e8a690fb5c24a7a0a14b1b2ca6cbc2cd8a79e25
cplank <cplankshepard@gmail.com>
2019-04-26 15:09:57 -0700

    Updates

---
+++ precache-manifest.47435d7cc8c9f367b4cb8d1993ffe13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7bb8695f5c18ee4ee50d036c3741a8",
+    "url": "/clickgame2/index.html"
+  },
+  {
+    "revision": "3387dd05cc5c9eccf818",
+    "url": "/clickgame2/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "2a030385037c72458674",
+    "url": "/clickgame2/static/js/2.07af2cd4.chunk.js"
+  },
+  {
+    "revision": "3387dd05cc5c9eccf818",
+    "url": "/clickgame2/static/js/main.3d68b8df.chunk.js"
+  },
+  {
+    "revision": "b518d53e00318990351c",
+    "url": "/clickgame2/static/js/runtime~main.70dd6d19.js"
+  }
+]);
